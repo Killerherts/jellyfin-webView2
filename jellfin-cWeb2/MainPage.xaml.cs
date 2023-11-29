b28d9de7bbe0704ee9cd316c58cc690ff402df12
Killerherts <20962973+Killerherts@users.noreply.github.com>
2023-11-28 23:13:43 -0600

    Webview2 Working with Static page

--- jellfin-cWeb2/MainPage.xaml.cs
+++ jellfin-cWeb2/MainPage.xaml.cs
@@ -1,30 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using Windows.Gaming.Input;
+using Windows.UI.Core;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
 
 namespace jellfin_cWeb2
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Gamepad _gamepad = null;
+
         public MainPage()
         {
             this.InitializeComponent();
+            Gamepad.GamepadAdded += Gamepad_GamepadAdded;
+            Gamepad.GamepadRemoved += Gamepad_GamepadRemoved;
+            Window.Current.CoreWindow.KeyDown += CoreWindow_KeyDown;
+        }
+
+        private void Gamepad_GamepadAdded(object sender, Gamepad e)
+        {
+            if (_gamepad == null)
+            {
+                _gamepad = e;
+                Task.Run(() => GamepadInputLoop());
+            }
+        }
+
+        private void Gamepad_GamepadRemoved(object sender, Gamepad e)
+        {
+            if (_gamepad == e)
+            {
+                _gamepad = null;
+            }
+        }
+
+        private async void GamepadInputLoop()
+        {
+            while (_gamepad != null)
+            {
+                var reading = _gamepad.GetCurrentReading();
+
+                if (reading.Buttons.HasFlag(GamepadButtons.A))
+                {
+                    await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+                    {
+                        await WebView2.ExecuteScriptAsync("document.activeElement.click();");
+                    });
+                }
+                else if (reading.Buttons.HasFlag(GamepadButtons.B))
+                {
+                    await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    {
+                        if (WebView2.CanGoBack) WebView2.GoBack();
+                    });
+                }
+
+                await Task.Delay(100); // Delay to prevent rapid firing
+            }
+        }
+
+        private void CoreWindow_KeyDown(CoreWindow sender, KeyEventArgs args)
+        {
+            // Handle key inputs if needed
         }
     }
 }
